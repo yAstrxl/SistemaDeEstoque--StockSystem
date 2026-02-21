3810dad7b96df73e24cfb59efee277a5f2068f59
yAstrxl <ArthurSMendes2710@proton.me>
2026-02-20 21:33:07 -0300

    Redirecionamento da pagina de login

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -5,20 +5,36 @@
 package login;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
  * @author 232.004836
  */
+@WebServlet("/login")
 public class Login extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
     
     protected void doPost(HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException{
             
     String usuario = request.getParameter("users");
+    String senha = request.getParameter("passw");
+    
+    response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        if("ADM".equals(usuario) && "1234".equals(senha)){
+        response.sendRedirect("test.html");
+        // out.println("<h2> Login realizado </h2>");
+        }else { 
+            out.println("<h2> Usuario ou Senha Errado </h2>");
+        }
    }
 }
