31a6ef3492f60dcc3172269f8dbe2435001703b9
yAstrxl <ArthurSMendes2710@proton.me>
2026-03-26 21:28:48 -0300

    Implantação funcional da api Viacep

--- src/main/java/model/ProdutoModel.java
+++ src/main/java/model/ProdutoModel.java
@@ -20,4 +20,79 @@ public class ProdutoModel {
     private long quantidade;
     private String valor;
     private String total;
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDataFabricacao() {
+        return dataFabricacao;
+    }
+
+    public void setDataFabricacao(String dataFabricacao) {
+        this.dataFabricacao = dataFabricacao;
+    }
+
+    public String getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(long quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+    
+    
 }   
+
--- src/main/webapp/js/cep.js
+++ src/main/webapp/js/cep.js
@@ -1,57 +1,82 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
-
-document.addEventListener("DOMContentLoaded", function(){
-    const campoCep = document.getElementById("cep");
-    
-    campoCep.addEventListener("blur",buscarCep);
-    campoCep.addEventListener("keypress",function(e){
-        if(e.key === "Enter"){
-            e.preventDefault();
-            buscarCep();
-        }
-    });
+document.addEventListener("DOMContentLoaded", function() {
+   const campoCep = document.getElementById("cep");
+   
+   campoCep.addEventListener("blur", buscarCep);
+   campoCep.addEventListener("keypress", function(e) {
+       if (e.key === "Enter") {
+           e.preventDefault();
+           buscarCep;
+       }
+   });
 });
 
-async function buscarCep(){
+async function buscarCep() {
     const campoCep = document.getElementById("cep");
     
     const cep = campoCep.value.replace(/\D/g, "");
     
-    if(cep.length !== 8){
-        if(cep.length < 0){
-            alert("o seu cep está incorreto.");
-            limparCampoEndereco();
+    if(cep.length !== 8) {
+        if(cep.length < 0) {
+            alert("O seu CEP está incorreto.");
+            limparCamposEndereco();
         }
         return;
     }
     
-    campoCep.style.borderColor= '#aaa';
-    preencherCampo({aguardando:true});
+    campoCep.style.borderColor = '#aaa';
+    preencherCampo({aguardando: true});
     
     try {
-        const apiCep = await fetch('https://viacep.com.br/ws/{cep}/json/');
-        const dados = await apiCep.json();
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const dados = await response.json();
         
-        if(dados.erro){
-            alert("Cep não encontrado.");
-            limparCampoEndereco();
-            campoCep.style.borderColor="red";
+        if (dados.erro) {
+            alert("CEP não encontrado.");
+            limparCamposEndereco();
+            campoCep.style.borderColor = "red";
             return;
         }
         
         preencherCampo(dados);
-        campoCep.style.borderColor="green";
-        campoCep.value=cep.replace(/(\d{5}) (\d{3})/,"$1-$2");
-    } catch (error){
+        campoCep.style.bordercolor = "green";
+        
+        campoCep.value = cep.replace(/(\d{5}) (\d{3})/, "$1-$2");
+        
+        document.getElementById("numero"); 
+        
+    } catch (error) {
         alert("Verifique a conexão com a internet");
         limparCamposEndereco();
-        campoCep.style.borderColor="red";
-        console.error("Erro na busca do CEP:",error);
+        campoCep.style.borderColor = "red";
+        console.error('Erro na busca do CEP: ', error);
+        
     }
 }
 
+function preencherCampo(dados){
+    if (dados.aguardando){
+        document.getElementById("cidade").value = "Buscando CPF...";
+        document.getElementById("endereco").value = "Buscando CPF...";
+        document.getElementById("bairro").value = "Buscando CPF...";
+        document.getElementById("estado").value = "Buscando CPF...";
+        return;
+    }
+    
+    document.getElementById("cidade").value = dados.localidade || "";
+    document.getElementById("endereco").value = dados.logradouro || "";
+    document.getElementById("bairro").value = dados.bairro || "";
+    document.getElementById("estado").value = dados.uf || "";
+    
+    if(dados.complemento){
+         document.getElementById("complemento").value = dados.complemento;
+    }
+}
 
-
+function limparCamposEndereco(){
+    document.getElementById("cidade").value = "";
+    document.getElementById("endereco").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("complemento").value = "";
+    document.getElementById("numero").value = "";
+    document.getElementById("estado").value = "";
+}
